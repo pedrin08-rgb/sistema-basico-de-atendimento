f5b84b86a09bb1d21b44db483ab6dee3ad2b5a5c
pedrin08-rgb <reginaldopedro4444@gmail.com>
2026-02-04 15:05:40 -0300

    usuario

---
+++ usuario.cs
@@ -0,0 +1,16 @@
+namespace ProjetoChamados.Models
+{
+    public abstract class Usuario
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+
+        protected Usuario(int id, string nome, string email)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+        }
+    }
+}
