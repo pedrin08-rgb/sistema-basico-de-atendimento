73009c93bab540a47e162fda334617098e0312e3
pedrin08-rgb <reginaldopedro4444@gmail.com>
2026-02-04 15:14:26 -0300

    Categoria

---
+++ categoria.cs
@@ -0,0 +1,14 @@
+namespace ProjetoChamados.Models
+{
+    public class Categoria
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+
+        public Categoria(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+    }
+}
