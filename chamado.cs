68643e40b4f5de28a68d51ce09d9bb411202ad85
pedrin08-rgb <reginaldopedro4444@gmail.com>
2026-02-04 15:17:26 -0300

    Chamado

---
+++ chamado.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using ProjetoChamados.Interfaces;
+
+namespace ProjetoChamados.Models
+{
+    public class Chamado : IAtribuivel, IEncerravel
+    {
+        public int Id { get; private set; }
+        public string Titulo { get; private set; }
+        public string Descricao { get; private set; }
+        public string Status { get; private set; }
+
+        public Cliente Cliente { get; private set; }
+        public Tecnico Tecnico { get; private set; }
+        public Categoria Categoria { get; private set; }
+
+        public List<HistoricoChamado> Historico { get; private set; }
+
+        public Chamado(int id, string titulo, string descricao, Cliente cliente, Categoria categoria)
+        {
+            Id = id;
+            Titulo = titulo;
+            Descricao = descricao;
+            Cliente = cliente;
+            Categoria = categoria;
+            Status = "Aberto";
+            Historico = new List<HistoricoChamado>();
+
+            Historico.Add(new HistoricoChamado("Chamado aberto"));
+        }
+
+        public void AtribuirTecnico(Tecnico tecnico)
+        {
+            Tecnico = tecnico;
+            Status = "Em andamento";
+            Historico.Add(new HistoricoChamado("Técnico atribuído"));
+        }
+
+        public void Encerrar()
+        {
+            Status = "Encerrado";
+            Historico.Add(new HistoricoChamado("Chamado encerrado"));
+        }
+    }
+}
