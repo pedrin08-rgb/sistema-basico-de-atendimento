72831b5bf0f97a84d7c5176eb8b19ff4f39a4ba4
pedrin08-rgb <reginaldopedro4444@gmail.com>
2026-02-06 00:52:17 -0300

    Change namespace for Categoria class

--- categoria.cs
+++ categoria.cs
@@ -1,4 +1,4 @@
-namespace ProjetoChamados.Models
+namespace sistema-basico-de-atendimento.Models
 {
     public class Categoria
     {
@@ -12,3 +12,4 @@ namespace ProjetoChamados.Models
         }
     }
 }
+
